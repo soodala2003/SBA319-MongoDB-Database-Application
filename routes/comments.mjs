0d89356be91318ec8c4dca9b5a36ba730c8d805e
soodala2003 <soodala78@gmail.com>
2024-11-12 03:08:44 -0500

    updated comments.mjs

--- routes/comments.mjs
+++ routes/comments.mjs
@@ -4,12 +4,13 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-// Get a list of 50 comments since there are 41079 comments
+// Get all names that have commented
 router.get("/", async (req, res) => {
     let collection = await db.collection("comments");
-    let results = await collection.find({}).limit(50).toArray();
+    let results = await collection.find({}).toArray();
 
-    res.send(results).status(200);
+    //res.send(results).status(200);
+    res.render("comments", { results});
 });
 
 // Get a single comment
@@ -19,7 +20,8 @@ router.get("/:id", async (req, res) => {
     let result = await collection.findOne(query);
 
     if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
+    else res.render("comments_id", { result });
+    //else res.send(result).status(200);
 });
 
 export default router;
