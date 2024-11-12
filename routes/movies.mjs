ec2dc131766d73b1309609a2d83260fdb7dc16c8
soodala2003 <soodala78@gmail.com>
2024-11-12 03:07:49 -0500

    updated movies.mjs

--- routes/movies.mjs
+++ routes/movies.mjs
@@ -4,17 +4,16 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-// Get a list of 50 movies with "_id", "plot", "tilte", and "year" entries 
+// Get all movies with "_id", "plot", "tilte", and "year" entries 
 router.get("/", async (req, res) => {
     let collection = await db.collection("movies");
     let results = await collection
         .aggregate([
             { $project: { _id: 1, plot: 1, title: 1, year: 1 } },
-            { $limit: 50 },
         ])
         .toArray();
-
-    res.send(results).status(200);
+    res.render("movies", { results });
+    //res.send(results).status(200);
 });
 
 // Get a single movie
