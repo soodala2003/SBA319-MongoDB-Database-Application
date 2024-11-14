4634dcfe360570d5e7359e1fdf5279e40e340cff
soodala2003 <soodala78@gmail.com>
2024-11-13 19:12:21 -0500

    updated users.mjs

--- routes/users.mjs
+++ routes/users.mjs
@@ -5,14 +5,12 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 
 // Get all users entries
-// Get 20 users with "_id", "name", "email"
 router.get("/", async (req, res) => {
     let collection = await db.collection("users");
     let results = await collection
         .aggregate([
             { $project: { _id: 1, name: 1, email: 1 } },
-        ])
-        .limit(20)
+        ]) 
         .toArray();
     res.render("users", { results });
 }); 
@@ -25,7 +23,6 @@ router.get("/:id", async (req, res) => {
 
     if (!result) res.send("Not found").status(404);
     else res.render("user_id", { result });
-    //else res.send(result).status(200);
 });
 
 // Add a new user to the collection
@@ -34,8 +31,7 @@ router.post("/", async (req, res) => {
     let newDocument = req.body;
     newDocument.date = new Date();
     let result = await collection.insertOne(newDocument);
-    //res.send(result).status(204);
-    //res.send({ message: "User Created", newDocument, result }).status(201);
+
     res.redirect("users");
 });
 
@@ -43,29 +39,26 @@ router.post("/", async (req, res) => {
 router.patch("/:id", async (req, res) => {
     const query = { _id: new ObjectId(req.params.id) };
     const updates = {
-      $push: { 
+      $set: { 
         name: req.body.name, 
         email: req.body.email, 
-        date: new Date() 
       },
     };
   
-    let collection = await db.collection("comments");
+    let collection = await db.collection("users");
     let result = await collection.updateOne(query, updates);
   
-    //res.send(result).status(200);
-    res.redirect(`/${query}`);
+    res.redirect("/users"); 
 });
 
 // Delete an entry
 router.delete("/:id", async (req, res) => {
-    const query = { _id: ObjectId(req.params.id) };
+    const query = { _id: new ObjectId(req.params.id) };
   
     const collection = db.collection("users");
     let result = await collection.deleteOne(query);
-  
-    res.send(result).status(200);
-    //res.render("user_id");
+
+    res.redirect("/users");
 });
 
 export default router;
