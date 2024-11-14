373bd7855ed3198bbd0c0788a8568ad7acebe3fb
soodala2003 <soodala78@gmail.com>
2024-11-13 19:11:00 -0500

    updated movies.mjs

--- routes/movies.mjs
+++ routes/movies.mjs
@@ -4,14 +4,13 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-// Get 20 movies with "_id", "plot", "tilte", and "year" entries 
+// Get all movies entries 
 router.get("/", async (req, res) => {
     let collection = await db.collection("movies");
     let results = await collection
         .aggregate([
             { $project: { _id: 1, plot: 1, title: 1, year: 1 } },
         ])
-        .limit(20)
         .toArray();
     res.render("movies", { results });
 });
@@ -23,7 +22,44 @@ router.get("/:id", async (req, res) => {
     let result = await collection.findOne(query);
 
     if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
+    else res.render("movie_id", { result });
+});
+
+// Add a new movie to the collection
+router.post("/", async (req, res) => {
+    let collection = await db.collection("movies");
+    let newDocument = req.body;
+    newDocument.date = new Date();
+    let result = await collection.insertOne(newDocument);
+
+    res.redirect("movies");
+});
+
+// Update a movie data
+router.patch("/:id", async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+    const updates = {
+      $set: { 
+        title: req.body.title, 
+        plot: req.body.plot, 
+        year: req.body.year,
+      },
+    };
+  
+    let collection = await db.collection("movies");
+    let result = await collection.updateOne(query, updates);
+  
+    res.redirect("/movies"); 
+});
+
+// Delete an entry
+router.delete("/:id", async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+  
+    const collection = db.collection("movies");
+    let result = await collection.deleteOne(query);
+
+    res.redirect("/movies");
 });
 
 export default router;
